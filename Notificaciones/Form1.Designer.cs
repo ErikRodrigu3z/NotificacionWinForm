6a4e1190e166fc4ffb9461ee86e89796cd55601c
erik <erik@DEV01>
2019-10-18 00:11:46 -0600

    Se crea proyecto

--- Notificaciones/Form1.Designer.cs
+++ Notificaciones/Form1.Designer.cs
@@ -29,12 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(237, 125);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Notificaciones/Form1.cs
+++ Notificaciones/Form1.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tulpep.NotificationWindow;
 
 namespace Notificaciones
 {
     public partial class Form1 : Form
     {
+        // Install-Package Tulpep.NotificationWindow
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        public void Toasty()
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.Image = Properties.Resources.toasty;
+            popup.ImageSize = new Size(400, 400);
+            popup.ImagePadding = new Padding(0);
+
+            popup.IsRightToLeft = true;
+            popup.ShowOptionsButton = false;
+            popup.ContentText = "";
+
+            popup.HeaderHeight = 1;
+            popup.HeaderColor = Color.Transparent;
+            popup.BodyColor = Color.Transparent;
+            popup.BodyColor = Color.Transparent;
+
+            popup.ButtonBorderColor = Color.Transparent;
+            popup.AnimationDuration = 200;
+            popup.AnimationInterval = 1;
+            popup.Delay = 3000;
+            popup.ShowGrip = false;
+
+            popup.ShowOptionsButton = false;
+            popup.Scroll = true;
+            popup.Size = new System.Drawing.Size(400, 400);
+            popup.ContentPadding = new Padding(1000);
+            popup.Popup();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Toasty();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 9000;
         }
     }
 }
